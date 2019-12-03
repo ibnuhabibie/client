f0432e37dd5b85f428f3b0934426985564feb4a4
mauricius <maurizio.bonani@gmail.com>
2019-12-03 11:47:27 +0100

    Add support for csrf token

--- resources/views/navigator.blade.php
+++ resources/views/navigator.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ $title }}</title>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css">
